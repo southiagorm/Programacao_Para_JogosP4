8f9f8d4d7bb4018640fddf4a12f6200c1badb6f2
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-09-04 13:52:34 -0300

    Criação do Projeto de Nave, GameObjects e Input

---
+++ ProjetoNaveV0.1/Assets/Scripts/Nave.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    public float speed = 0.01f;
+    private float _h, _v;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //transform.Translate(0,0.01f,0);
+        //transform.Rotate(0, 0, 1);
+
+        /*if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,speed,0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0,-speed,0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }*/
+
+        _h = Input.GetAxisRaw("Horizontal");
+        _v = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(_h*speed,_v*speed, 0);
+    }
+}
