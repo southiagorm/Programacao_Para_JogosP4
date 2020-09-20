e0d171ffcadf5806e6e5703aebd2467a5044d4aa
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-09-19 22:20:18 -0300

    Aula 17/09 - Colisões, Time

--- ProjetoNaveV0.1/Assets/Scripts/Laser.cs
+++ ProjetoNaveV0.1/Assets/Scripts/Laser.cs
@@ -20,11 +20,42 @@ public class Laser : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rb2dBody.velocity = new Vector2(0,speed*Time.deltaTime);
+        //_rb2dBody.velocity = new Vector2(0,speed*Time.deltaTime);
+        _rb2dBody.MovePosition(_rb2dBody.position + new Vector2(0, speed * Time.deltaTime));
     }
 
     void OnBecameInvisible()
     {
         Destroy(this.gameObject);    
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        print("acabou de colidir");
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        print("continua colidindo");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("enter trigger");
+        if(other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        print("stay trigger");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        print("exit trigger");
     }
 }
--- ProjetoNaveV0.1/Assets/Scripts/Nave.cs
+++ ProjetoNaveV0.1/Assets/Scripts/Nave.cs
@@ -12,6 +12,9 @@ public class Nave : MonoBehaviour
 
     private Rigidbody2D _rb2dBody;
 
+    public float timeRate = 0;
+    public float maxTime = 1;
+
     void Start()
     {
         _rb2dBody = GetComponent<Rigidbody2D>();
@@ -23,14 +26,43 @@ public class Nave : MonoBehaviour
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
 
+        timeRate += Time.deltaTime;
+
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(prefabBala, transform.position + new Vector3(0,1,0), transform.rotation);
+            if (timeRate >= maxTime)
+            {
+                Instantiate(prefabBala, transform.position + new Vector3(0, 1, 0), transform.rotation);
+                timeRate = 0;
+            }
+        }
+
+        if(transform.position.x >= 10.0f)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x <= -10.0f)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        if(transform.position.y >= 2.2f)
+        {
+            transform.position = new Vector3(transform.position.x, 2.2f, transform.position.z);
+        }
+        if (transform.position.y <= -4.4f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.4f, transform.position.z);
         }
     }
 
     void FixedUpdate()
     {
-        _rb2dBody.velocity = new Vector2(_h * speed * Time.deltaTime, _v * speed * Time.deltaTime);
+        //_rb2dBody.velocity = new Vector2(_h * speed * Time.deltaTime, _v * speed * Time.deltaTime);
+        _rb2dBody.MovePosition(_rb2dBody.position + (new Vector2(_h,_v) * speed * Time.deltaTime));
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        print("Deixou de colidir");
     }
 }
