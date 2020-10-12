3755058b8633cd7ef37d64de2b90410d22934379
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-10-11 22:25:58 -0300

    Aula 08/10 - Resolução de exercício dungeon

---
+++ ProjetoDungeon/Assets/Scripts/GeradorDeMoedas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeMoedas : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabMoeda;
+
+    [SerializeField]
+    private float time;
+    [SerializeField]
+    private float timeRate;
+
+    void Start()
+    {
+        InvokeRepeating("GerarMoeda", time, timeRate);
+    }
+
+    public void GerarMoeda()
+    {
+        float x = Random.Range(-10.8f, 10.8f);
+        float y = Random.Range(-4.8f, 4.8f);
+
+        Instantiate(prefabMoeda, new Vector2(x,y), Quaternion.identity);
+    }
+}
---
+++ ProjetoDungeon/Assets/Scripts/Knight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private float _h, _v;
+    private Rigidbody2D rb2dBody;
+
+    [SerializeField]
+    private int pontuacao;
+    
+    void Start()
+    {
+        rb2dBody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        _h = Input.GetAxisRaw("Horizontal");
+        _v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb2dBody.MovePosition(rb2dBody.position + (new Vector2(_h, _v)*speed*Time.deltaTime));    
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Moeda"))
+        {
+            pontuacao += 1;
+            Destroy(other.gameObject);
+        }  
+    }
+}
