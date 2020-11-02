ab47ef7d1bd6331ef5c20f3cbc21a32c51674eb3
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-11-02 11:27:11 -0300

    Aula 29/11 - Projeto Jogo de Plataforma

---
+++ ProjetoPlataformaV0.1/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    
+    
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private float h;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+
+        anim.SetFloat("run", Mathf.Abs(h));
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(h * speed * Time.deltaTime, rb2d.velocity.y);
+    }
+}
