8f9f91a5a0e523703651b451ab9f828d59d26630
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-11-22 00:17:48 -0300

    Aula 19/11 - Criação da mecânica de vida

---
+++ ProjetoPlataformaV0.4/Assets/Scripts/Spike.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerLife.instance.Damage(1);
+        }
+    }
+}
---
+++ ProjetoPlataformaV0.4/Assets/Scripts/UserInterfaceController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterfaceController : MonoBehaviour
+{
+    public static UserInterfaceController instance;
+
+    [SerializeField]
+    private Image[] uiHearts;
+
+    [SerializeField]
+    private Sprite fullHeart, emptyHeart;
+
+    void Awake()
+    {
+        instance = this;    
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLifeDisplay()
+    {
+        switch (PlayerLife.instance.Life)
+        {
+            case 4:
+                for(int i = 0; i < uiHearts.Length; i++)
+                {
+                    uiHearts[i].sprite = fullHeart;
+                }
+                break;
+            case 3:
+                for (int i = 0; i < uiHearts.Length-1; i++)
+                {
+                    uiHearts[i].sprite = fullHeart;
+                }
+                uiHearts[3].sprite = emptyHeart;
+                break;
+            case 2:
+                for (int i = 0; i < uiHearts.Length - 2; i++)
+                {
+                    uiHearts[i].sprite = fullHeart;
+                }
+                uiHearts[2].sprite = emptyHeart;
+                uiHearts[3].sprite = emptyHeart;
+                break;
+            case 1:
+                for (int i = 0; i < uiHearts.Length - 3; i++)
+                {
+                    uiHearts[i].sprite = fullHeart;
+                }
+                uiHearts[1].sprite = emptyHeart;
+                uiHearts[2].sprite = emptyHeart;
+                uiHearts[3].sprite = emptyHeart;
+                break;
+            case 0:
+                for (int i = 0; i < uiHearts.Length ; i++)
+                {
+                    uiHearts[i].sprite = emptyHeart;
+                }
+                break;
+        }
+    }
+}
